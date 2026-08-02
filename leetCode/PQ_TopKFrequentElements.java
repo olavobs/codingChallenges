445dbb5fc5f7d88987d9f1140b4e7ad95c9ea582
Olavo Brito <olavo.briso@gmail.com>
2026-08-02 21:56:41 +0200

    Fix Build

--- leetCode/PQ_TopKFrequentElements.java
+++ leetCode/PQ_TopKFrequentElements.java
@@ -21,28 +21,23 @@ public class PQ_TopKFrequentElements {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2)));
         System.out.println(Arrays.toString(topKFrequent(new int[]{1}, 1)));
-        System.out.println(Arrays.toString(topKFrequent(new int[]{1, 2, 1, 2, 1, 2, 3, 1, 3, 2}, 2)));
+        System.out.println(Arrays.toString(topKFrequent(new int[]{1, 2, 1, 2, 1, 2, 3, 1, 3, 2}, 3)));
     }
 
     public static int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> frequencyMap = new HashMap<>();
 
         for (int num : nums) {
-            if (!frequencyMap.containsKey(num)) {
-                frequencyMap.put(num, 1);
-            } else {
-                Integer i = frequencyMap.get(num);
-                frequencyMap.put(num, ++i);
-            }
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
 
-        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
-        queue.addAll(frequencyMap.entrySet());
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        minHeap.addAll(frequencyMap.entrySet());
 
         int[] answer = new int[k];
         int aux = 0;
         while (aux < k) {
-            answer[aux] = queue.poll().getKey();
+            answer[aux] = minHeap.poll().getKey();
             aux++;
         }
         return answer;
